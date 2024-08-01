93ead28be1debb686644eb3e9d5f8167b98c0759
JHLEE-STG-DEV <jhmobile2020@gmail.com>
2024-08-01 14:03:23 +0900

    Update entrypoint.sh

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /app
-nohup fluent-bit -c /etc/fluent-bit/fluent-bit.conf &
+nohup /opt/fluent-bit/bin/fluent-bit -c /etc/fluent-bit/fluent-bit.conf &
 java -Dspring.proriles.active=loc -jar pnc.jar
